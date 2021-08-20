c9d782778a9ed9c5bd5b8a5eb3e3dbba4680a9af
Hyeong Kyun (Daniel) Park <h1yungpark@gmail.com>
2021-08-19 19:19:04 -1000

    Update codeEditor.py
    Cleaned up & fixed code so it is deployable on Python IDES

--- codeEditor.py
+++ codeEditor.py
@@ -1,15 +1,19 @@
 from pywebio import *
 from pywebio.output import *
 from pywebio.input import *
-def main():
+
+if __name__ == '__main__':
     '''HTML Code Editor'''
+    
     # (1) header
     style(put_text("HTML Editor 💻"), 'text-align:center; font-size:200%, font-color:black')
+    
     # (2) output
     set_scope("scope")
     html = output()
     with use_scope('scope', clear=True):  # enter the existing scope and clear the previous content
         put_scrollable(html, height=400, keep_bottom=True)
+    
     # (3) input
     while True:
         data = input_group('Write Your HTML Code', [
@@ -26,7 +30,3 @@ def main():
         with use_scope('scope', clear=True):  # enter the existing scope and clear the previous content
             put_scrollable(html, height=400, keep_bottom=True)
         html.append(put_html(data['code']))
-
-from pywebio.platform.flask import start_server
-
-start_server(main, debug=False, port=37519, cdn=True)
